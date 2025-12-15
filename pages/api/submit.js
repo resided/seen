4fe98e6556d4e35e5041751b51a11734a9288c41
Reside <106778109+resided@users.noreply.github.com>
2025-12-15 15:44:28 +0000

    Fix submit API: add missing tokenName destructure from req.body

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
       description,
       builder,
       builderFid,
+      tokenName,
       category,
       submissionType,
       paymentAmount,
